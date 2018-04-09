cc83ea0531169f164f32b1918370fec3d2450944
shashankgd <34120423+shashankgd@users.noreply.github.com>
2018-04-09 17:49:15 +0530

    Add files via upload

---
+++ finProduct/property.py
@@ -0,0 +1,9 @@
+import os
+
+
+cd=os.getcwd()
+db_path=os.path.join(cd,'database')
+user_ip=os.path.join(db_path,'user_ip.csv')
+features=os.path.join(db_path,'features.csv')
+featuredata=os.path.join(db_path,'featuredata.csv')
+
